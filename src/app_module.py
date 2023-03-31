3e9401e2f221544721ba7b2717835ba54cdc606c
Aishen <aish3n@pm.me>
2023-03-31 17:40:47 +0800

    feat: render and navigate between screens

--- src/app_module.py
+++ src/app_module.py
@@ -1,10 +1,16 @@
 import pygame
-from .cmd_module import (QuitCommand, CommandType)
 from .mem_module import Memory
 from .gui_module import Display
+from .cmd_module import (
+    QuitCommand,
+    ModifyScreenPtrCommand,
+    CommandType
+)
 
 APP_FPS = 60
 APP_WINDOW_SIZE = (800, 600)
+APP_KEYDOWN_DELAY = 500
+APP_KEYDOWN_INTERVAL = 100
 
 
 class App:
@@ -27,6 +33,8 @@ class App:
         self.mem.init()
         self.gui.init()
 
+        pygame.key.set_repeat(APP_KEYDOWN_DELAY, APP_KEYDOWN_INTERVAL)
+
         return
 
     def listen(self):
@@ -37,6 +45,8 @@ class App:
             if event.type == pygame.QUIT:
                 self.cmd_queue.append(QuitCommand())
                 break
+            elif event.type == pygame.KEYDOWN:
+                on_keydown(event)
 
         return
 
@@ -51,6 +61,8 @@ class App:
             if command.type == CommandType.QUIT:
                 self.running = False
                 break
+            elif command.type == CommandType.MODIFY_SCREEN_PTR:
+                self.gui.screen_ptr = command.new_ptr
 
         self.cmd_queue.clear()
 
@@ -60,8 +72,14 @@ class App:
         if not self.running:
             return
 
-        self.screen.fill("black")
-        pygame.display.flip()
+        try:
+            screen = self.gui.screen_vec[self.gui.screen_ptr]
+            self.screen.fill(screen.bg_color)
+            pygame.display.flip()
+
+        except IndexError as err:
+            print("screen_ptr:", err)
+            exit(1)
 
         return
 
@@ -72,3 +90,26 @@ class App:
         self.delta = self.clock.tick(APP_FPS)
 
         return
+
+
+def on_keydown(event):
+    app = App()
+    mods = pygame.key.get_mods()
+
+    if event.key == pygame.K_h and mods & pygame.KMOD_CTRL:
+        new_ptr = app.gui.screen_ptr
+
+        if (app.gui.screen_ptr == 0):
+            new_ptr = len(app.gui.screen_vec) - 1
+        else:
+            new_ptr = app.gui.screen_ptr - 1
+
+        app.cmd_queue.append(ModifyScreenPtrCommand(new_ptr))
+
+    elif event.key == pygame.K_l and mods & pygame.KMOD_CTRL:
+        new_ptr = app.gui.screen_ptr + 1
+        new_ptr %= len(app.gui.screen_vec)
+
+        app.cmd_queue.append(ModifyScreenPtrCommand(new_ptr))
+
+    return
--- src/cmd_module.py
+++ src/cmd_module.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class CommandType(Enum):
     QUIT = 0
+    MODIFY_SCREEN_PTR = 1
 
 
 class Command:
@@ -13,3 +14,9 @@ class Command:
 class QuitCommand(Command):
     def __init__(self):
         super().__init__(CommandType.QUIT)
+
+
+class ModifyScreenPtrCommand(Command):
+    def __init__(self, new_ptr):
+        super().__init__(CommandType.MODIFY_SCREEN_PTR)
+        self.new_ptr = new_ptr
